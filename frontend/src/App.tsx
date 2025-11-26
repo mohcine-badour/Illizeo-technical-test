219e7db15a882c6780f378b8bc4d9957bc538020
BADOUR MOHCINE <79023454+mohcinebadour2000@users.noreply.github.com>
2025-11-26 19:15:36 +0100

    Add Profile page and update routing in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import ListNotes from "./pages/ListNotes";
 import MyNotes from "./pages/Mynotes";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/list-notes" element={<ListNotes />} />
         <Route path="/my-notes" element={<MyNotes />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
